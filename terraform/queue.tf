8becd047b4e700f351e7ebf8112a89ecca8e8094
Abhimanyu Haralukallu <32696360+AbhimanyuHK@users.noreply.github.com>
2026-08-26 08:26:30 +0530

    style: format Terraform queue resources

--- terraform/queue.tf
+++ terraform/queue.tf
@@ -1,17 +1,17 @@
 resource "aws_sqs_queue" "film_jobs_dlq" {
-  name = "${var.name_prefix}-${var.film_id}-jobs-dlq"
+  name              = "${var.name_prefix}-${var.film_id}-jobs-dlq"
   kms_master_key_id = aws_kms_key.film.arn
 }
 
 resource "aws_sqs_queue" "film_jobs" {
-  name = "${var.name_prefix}-${var.film_id}-jobs"
+  name                       = "${var.name_prefix}-${var.film_id}-jobs"
   visibility_timeout_seconds = 900
-  message_retention_seconds = 1209600
-  receive_wait_time_seconds = 20
-  kms_master_key_id = aws_kms_key.film.arn
+  message_retention_seconds  = 1209600
+  receive_wait_time_seconds  = 20
+  kms_master_key_id          = aws_kms_key.film.arn
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.film_jobs_dlq.arn
-    maxReceiveCount = 3
+    maxReceiveCount     = 3
   })
 }
 
@@ -21,13 +21,18 @@ resource "aws_iam_policy" "runtime_queue" {
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Action = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:ChangeMessageVisibility", "sqs:GetQueueAttributes"]
+      Action = [
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:ChangeMessageVisibility",
+        "sqs:GetQueueAttributes"
+      ]
       Resource = aws_sqs_queue.film_jobs.arn
     }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "runtime_queue" {
-  role = aws_iam_role.runtime.name
+  role       = aws_iam_role.runtime.name
   policy_arn = aws_iam_policy.runtime_queue.arn
 }
